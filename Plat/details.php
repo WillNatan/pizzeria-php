f6fcffee314a5665a5bf548852c685dfbb733ff8
WillNatan <willy97438@live.fr>
2020-11-19 13:11:21 +0400

    page détails

---
+++ Plat/details.php
@@ -0,0 +1,24 @@
+<?php
+include("../header.php");
+?>
+<section class="details-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+
+            </div>
+            <div class="col-md-6">
+                <div class="product-content">
+                    <h1>Rougail Saucisses</h1>
+                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        A voluptas, molestias labore quisquam quod animi natus quam minus quis,
+                        maxime modi vero suscipit expedita illum officiis qui voluptatum autem dolorem.</p>
+                    <p class="price"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include("../footer.php");
+?>
